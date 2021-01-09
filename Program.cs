14eb69571fd12e68abea50b12583d990a38f84fc
Latha1512 <77051923+Latha1512@users.noreply.github.com>
2021-01-09 15:46:31 +0530

    declaring integer variable

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Dotnetpractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int i=3;
+            Console.WriteLine("the value of i is {0}",i);
             Console.ReadLine();
         }
     }
