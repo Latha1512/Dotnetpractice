43f81c248a852ec99eae0e84678bdfdc67ef501f
Latha1512 <77051923+Latha1512@users.noreply.github.com>
2021-01-09 16:52:49 +0530

    datatypes and variables

--- Program.cs
+++ Program.cs
@@ -9,6 +9,18 @@ namespace Dotnetpractice
             Console.WriteLine("Hello World!");
             int i=3;
             Console.WriteLine("the value of i is {0}",i);
+            double d=459.9457;
+            Console.WriteLine("the value of d is {0}",d);
+            char key='A';
+            Console.WriteLine("the value of key is {0}",key);
+            string str="string";
+            Console.WriteLine("the value of str is {0}",str);
+            bool e=true;
+            Console.WriteLine("the value of e is {0}",e);
+            int r=(int)d;//implicit type casting
+            Console.WriteLine("the value of r is {0}",r);
+            double s=r;//explicit type casting
+            Console.WriteLine("the value of s is {0}",s);
             Console.ReadLine();
         }
     }
